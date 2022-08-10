5a9a7fb7498ad904966d9f68b104b7701a68be7f
lin <linkq@dreamdt.cn>
2022-08-10 15:42:43 +0800

    2.增加im 的客户端和服务端启动

---
+++ Netty_IM/src/main/java/com/example/server/ClientService.java
@@ -0,0 +1,55 @@
+package com.example.server;
+
+import com.example.decoder.MyDecoder;
+import com.example.encoder.MyEncoder;
+import com.example.handler.ClientChatHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+/**
+ * @Author: linK
+ * @Date: 2022/8/10 15:33
+ * @Description TODO
+ */
+public class ClientService {
+    public static void main(String[] args) throws Exception {
+        NioEventLoopGroup eventExecutors = new NioEventLoopGroup();
+        try {
+            //创建bootstrap对象，配置参数
+            Bootstrap bootstrap = new Bootstrap();
+            //设置线程组
+            bootstrap.group(eventExecutors)
+                    //设置客户端的通道实现类型
+                    .channel(NioSocketChannel.class)
+                    //使用匿名内部类初始化通道
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            //1.拆包器
+                            ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 5, 4));
+                            //2.自定义解码器
+                            ch.pipeline().addLast(new MyDecoder());
+                            //3.自定义业务  添加客户端通道的处理器
+                            ch.pipeline().addLast(new ClientChatHandler());
+                            //4.自定义编码器
+                            ch.pipeline().addLast(new MyEncoder());
+                        }
+                    });
+            System.out.println("客户端准备就绪，随时可以起飞~");
+            //连接服务端
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6666).sync();
+            //对通道关闭进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            //关闭线程组
+            eventExecutors.shutdownGracefully();
+        }
+    }
+
+
+}
